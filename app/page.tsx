25b044b22158702d25a8700baa7cad012b2b2acb
muhammed navas <muhammednavas6287@gmail.com>
2024-01-25 21:41:27 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      thi is my job portal web site 
+      this is my job portal web site 
     </div>
   );
 }
